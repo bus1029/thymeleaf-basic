1edfb450269d76cf46014f0711450261fb02fc85
Seokhyun Bae <bus1029@gmail.com>
2021-10-22 16:09:22 +0900

    feat: SpringEL 문법을 사용한 변수 접근
    - ThymeLeaf에서 SpringEL 문법을 사용해서 Controller에서 저장한 list 및 map 구조에서
    객체를 꺼내 특정 변수에 접근할 수 있음

--- src/main/kotlin/hello/thymeleaf/basic/BasicController.kt
+++ src/main/kotlin/hello/thymeleaf/basic/BasicController.kt
@@ -19,4 +19,23 @@ class BasicController {
     model.addAttribute("data", "Hello <b> Spring! </b>")
     return "basic/text-unescaped"
   }
+
+  @GetMapping("/variable")
+  fun variable(model: Model): String {
+    val userA = User("userA", 10)
+    val userB = User("userB", 20)
+
+    val users = listOf(userA, userB)
+    val map = mapOf("userA" to userA, "userB" to userB)
+
+    model.addAttribute("user", userA)
+    model.addAttribute("users", users)
+    model.addAttribute("userMap", map)
+
+    return "basic/variable"
+  }
+
+  class User constructor(val username: String, val age: Int) {
+
+  }
 }
